044161d5a980b4c95e1d3b3296ef598a47aaa5b3
Dillon Redding <dillon@global.id>
2021-11-04 16:35:47 -0500

    other(IDENTPD-123): exposing credentials via getters

--- src/utils/access-token-provider.ts
+++ src/utils/access-token-provider.ts
@@ -1,15 +1,28 @@
 import * as auth from '../services/auth';
 
 export class AccessTokenProvider {
-  constructor(readonly clientId: string, readonly clientSecret: string, gidApiHost: string = 'api.global.id') {
+  #clientId: string;
+  #clientSecret: string;
+
+  constructor(clientId: string, clientSecret: string, gidApiHost: string = 'api.global.id') {
     // TODO: validate parameters
+    this.#clientId = clientId;
+    this.#clientSecret = clientSecret;
     auth.init(`https://${gidApiHost}`);
+  }
+
+  get clientId(): string {
+    return this.#clientId;
+  }
+
+  get clientSecret(): string {
+    return this.#clientSecret;
   }
 
   async getAccessToken(): Promise<string> {
     const authTokenResponse: auth.AuthTokenResponse = await auth.issueTokens({
-      client_id: this.clientId,
-      client_secret: this.clientSecret,
+      client_id: this.#clientId,
+      client_secret: this.#clientSecret,
       grant_type: 'client_credentials'
     });
     return authTokenResponse.access_token;
