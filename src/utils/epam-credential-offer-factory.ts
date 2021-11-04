f4ca39ca58a2ab99703fd6b6dac4f2fc47bed243
Dillon Redding <dillon@global.id>
2021-11-04 16:35:15 -0500

    other(IDENTPD-123): adding separate test for factory

--- src/utils/epam-credential-offer-factory.ts
+++ src/utils/epam-credential-offer-factory.ts
@@ -1,12 +1,14 @@
-import { Claims, ClaimValue, GidCredentialOffer } from '../common';
+import { Claims, ClaimValue, GidCredentialOffer, PrimitiveClaimValueType } from '../common';
 import * as epam from '../services/epam';
 
 export function createEpamCredentialOffer(offer: GidCredentialOffer): epam.EpamCreateCredentialsOfferV2 {
   return {
     thread_id: offer.threadId,
+    name: offer.title,
+    description: offer.description,
     schema_url: offer.contextIri,
     schema_type: offer.subjectType,
-    credential_attributes: createCredentialAttributes(offer.claims)
+    attributes: createCredentialAttributes(offer.claims)
   };
 }
 
@@ -18,8 +20,27 @@ function createCredentialAttribute(name: string, claimValue: ClaimValue): epam.C
   return {
     name,
     value: String(claimValue.value),
-    mime_type: claimValue.type
+    value_type: toEpamValueType(claimValue.type)
   };
+}
+
+function toEpamValueType(type: PrimitiveClaimValueType): epam.ValueType {
+  switch (type) {
+    case PrimitiveClaimValueType.Boolean:
+      return epam.ValueType.boolean;
+    case PrimitiveClaimValueType.Integer:
+      return epam.ValueType.integer;
+    case PrimitiveClaimValueType.Number:
+      return epam.ValueType.number;
+    case PrimitiveClaimValueType.String:
+      return epam.ValueType.string;
+    case PrimitiveClaimValueType.Date:
+      return epam.ValueType.date;
+    case PrimitiveClaimValueType.Time:
+      return epam.ValueType.time;
+    case PrimitiveClaimValueType.DateTime:
+      return epam.ValueType.date_time;
+  }
 }
 
 export default createEpamCredentialOffer;
