02695e045ce1c7d2cc151d105a7dd5921d0c40d2
Dillon Redding <dillon@global.id>
2021-11-11 09:13:52 -0600

    feat(IDENTPD-163): sha512sum, JSDoc, and const change

--- src/utils/crypto.spec.ts
+++ src/utils/crypto.spec.ts
@@ -2,12 +2,13 @@ import * as gidCrypto from 'globalid-crypto-library';
 import { mocked } from 'ts-jest/utils';
 
 import { publicKey } from '../../test/stubs';
-import { AES_KEY_LENGTH, encrypt } from './crypto';
+import { AES_KEY_LENGTH_IN_BYTES, encrypt, sha512sum } from './crypto';
 
 jest.mock('globalid-crypto-library');
 
 const mockedAesEncryptBuffer = mocked(gidCrypto.AES.encryptBuffer);
 const mockedBytesToHex = mocked(gidCrypto.Util.bytesToHex);
+const mockedHashSha512 = mocked(gidCrypto.Util.hashSHA512);
 const mockedRandomBytes = mocked(gidCrypto.Util.randomBytes);
 const mockedRsaEncrypt = mocked(gidCrypto.RSA.encrypt);
 
@@ -29,12 +30,26 @@ describe('encrypt', () => {
     expect(result[0]).toBe(ciphertext);
     expect(result[1]).toEqual(encryptedKey);
     expect(mockedRandomBytes).toHaveBeenCalledTimes(1);
-    expect(mockedRandomBytes).toHaveBeenCalledWith(AES_KEY_LENGTH / 8);
+    expect(mockedRandomBytes).toHaveBeenCalledWith(AES_KEY_LENGTH_IN_BYTES);
     expect(mockedBytesToHex).toHaveBeenCalledTimes(1);
     expect(mockedBytesToHex).toHaveBeenCalledWith(randomBytes);
     expect(mockedAesEncryptBuffer).toHaveBeenCalledTimes(1);
     expect(mockedAesEncryptBuffer).toHaveBeenCalledWith(plaintext, key);
     expect(mockedRsaEncrypt).toHaveBeenCalledTimes(1);
     expect(mockedRsaEncrypt).toHaveBeenCalledWith(publicKey, key);
+  });
+});
+
+describe('sha512sum', () => {
+  it('should delegate to globalid-crypto-library', () => {
+    const data = Buffer.from('Lorem ipsum dolor sit amet');
+    const output = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    mockedHashSha512.mockReturnValueOnce(output);
+
+    const result = sha512sum(data);
+
+    expect(result).toBe(output);
+    expect(mockedHashSha512).toHaveBeenCalledTimes(1);
+    expect(mockedHashSha512).toHaveBeenCalledWith(data);
   });
 });
--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,10 +1,26 @@
 import { AES, RSA, Util } from 'globalid-crypto-library';
 
-export const AES_KEY_LENGTH = 256;
+export const AES_KEY_LENGTH_IN_BYTES = 32;
 
+/**
+ * Encrypts the given plaintext using AES and a randomly-generated 256-bit key, which is returned—along with the
+ * ciphertext—after being encrypted using RSA and the provided asymmetric public key.
+ * @param plaintext Data to be encrypted
+ * @param publicKey Asymmetric public key used to encrypt the generated symmetric key
+ * @returns Ciphertext and encrypted symmetric key as a `Buffer`-`string` pair
+ */
 export function encrypt(plaintext: Buffer, publicKey: string): [Buffer, string] {
-  const key = Util.bytesToHex(Util.randomBytes(AES_KEY_LENGTH / 8));
+  const key = Util.bytesToHex(Util.randomBytes(AES_KEY_LENGTH_IN_BYTES));
   const ciphertext = AES.encryptBuffer(plaintext, key);
   const encryptedKey = RSA.encrypt(publicKey, key);
   return [ciphertext, encryptedKey];
 }
+
+export function sha512sum(data: Buffer): string {
+  return Util.hashSHA512(data);
+}
+
+export default {
+  encrypt,
+  sha512sum
+};
