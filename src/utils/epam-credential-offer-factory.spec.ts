270c017492011967c7799145bcdd2512c5cc7225
Dillon Redding <dillon@global.id>
2021-11-18 16:23:08 -0600

    feat(IDENTPD-295): schema URI and other EPAM changes

--- src/utils/epam-credential-offer-factory.spec.ts
+++ src/utils/epam-credential-offer-factory.spec.ts
@@ -1,119 +1,30 @@
 import { threadId } from '../../test/stubs';
-import { ClaimValueType, FileType, CredentialOffer } from '../common';
-import { ValueType } from '../services/epam';
+import { FileType, CredentialOffer } from '../common';
 import createEpamCredentialOffer from './epam-credential-offer-factory';
 
 const name = 'Foo Bar';
 const description = 'Lorem ipsum dolor sit amet';
-const contextIri = 'https://example.com/context';
 const subjectType = 'Foo';
+const contextUri = 'https://example.com/context';
+const schemaUri = 'https://example.com/schema';
 
 test('should transform GidCredentialOffer into EPAM credential offer', () => {
   const offer: CredentialOffer = {
     threadId,
     name,
     description,
-    contextIri,
     subjectType,
+    contextUri,
+    schemaUri,
     claims: {
-      boolean: {
-        type: ClaimValueType.Boolean,
-        value: false
-      },
-      integer: {
-        type: ClaimValueType.Integer,
-        value: 42
-      },
-      number: {
-        type: ClaimValueType.Number,
-        value: 42.0
-      },
-      string: {
-        type: ClaimValueType.String,
-        value: 'foobar'
-      },
-      date: {
-        type: ClaimValueType.Date,
-        value: '1970-01-01'
-      },
-      time: {
-        type: ClaimValueType.Time,
-        value: '00:00:00Z'
-      },
-      dateTime: {
-        type: ClaimValueType.DateTime,
-        value: '1970-01-01T00:00:00Z'
-      }
-    }
-  };
-
-  const result = createEpamCredentialOffer(offer);
-
-  expect(result).toEqual({
-    thread_id: threadId,
-    name: name,
-    description,
-    schema_url: contextIri,
-    schema_type: subjectType,
-    attributes: [
-      {
-        name: 'boolean',
-        value: 'false',
-        value_type: ValueType.boolean
-      },
-      {
-        name: 'integer',
-        value: '42',
-        value_type: ValueType.integer
-      },
-      {
-        name: 'number',
-        value: '42',
-        value_type: ValueType.number
-      },
-      {
-        name: 'string',
-        value: 'foobar',
-        value_type: ValueType.string
-      },
-      {
-        name: 'date',
-        value: '1970-01-01',
-        value_type: ValueType.date
-      },
-      {
-        name: 'time',
-        value: '00:00:00Z',
-        value_type: ValueType.time
-      },
-      {
-        name: 'dateTime',
-        value: '1970-01-01T00:00:00Z',
-        value_type: ValueType.date_time
-      }
-    ]
-  });
-});
-
-test('should string file claims', () => {
-  const offer: CredentialOffer = {
-    threadId,
-    name,
-    description,
-    contextIri,
-    subjectType,
-    claims: {
-      jpegFile: {
+      boolean: false,
+      number: 42,
+      string: 'foobar',
+      file: {
         type: FileType.JPEG,
         decryptionKey: 'foobar',
         sha512sum: 'abcdefg',
         url: 'https://example.com/some-file'
-      },
-      pngFile: {
-        type: FileType.PNG,
-        decryptionKey: 'bazqux',
-        sha512sum: 'hijklmn',
-        url: 'https://example.com/another-file'
       }
     }
   };
@@ -124,71 +35,19 @@ test('should string file claims', () => {
     thread_id: threadId,
     name: name,
     description,
-    schema_url: contextIri,
-    schema_type: subjectType,
-    attributes: [
-      {
-        name: 'jpegFile',
-        value_type: ValueType.image_jpeg,
-        value: JSON.stringify({
-          decryption_key: 'foobar',
-          media_type: 'image/jpeg',
-          sha_512_sum: 'abcdefg',
-          url: 'https://example.com/some-file'
-        })
-      },
-      {
-        name: 'pngFile',
-        value_type: ValueType.image_png,
-        value: JSON.stringify({
-          decryption_key: 'bazqux',
-          media_type: 'image/png',
-          sha_512_sum: 'hijklmn',
-          url: 'https://example.com/another-file'
-        })
-      }
-    ]
-  });
-});
-
-test('should infer primitive value types', () => {
-  const offer: CredentialOffer = {
-    threadId,
-    name,
-    description,
-    contextIri,
-    subjectType,
-    claims: {
+    subject_type: subjectType,
+    context_uri: contextUri,
+    schema_uri: schemaUri,
+    attributes: {
       boolean: false,
       number: 42,
-      string: 'foobar'
-    }
-  };
-
-  const result = createEpamCredentialOffer(offer);
-
-  expect(result).toEqual({
-    thread_id: threadId,
-    name: name,
-    description,
-    schema_url: contextIri,
-    schema_type: subjectType,
-    attributes: [
-      {
-        name: 'boolean',
-        value: 'false',
-        value_type: ValueType.boolean
-      },
-      {
-        name: 'number',
-        value: '42',
-        value_type: ValueType.number
-      },
-      {
-        name: 'string',
-        value: 'foobar',
-        value_type: ValueType.string
+      string: 'foobar',
+      file: {
+        media_type: FileType.JPEG,
+        decryption_key: 'foobar',
+        sha_512_sum: 'abcdefg',
+        url: 'https://example.com/some-file'
       }
-    ]
+    }
   });
 });
--- src/utils/epam-credential-offer-factory.ts
+++ src/utils/epam-credential-offer-factory.ts
@@ -1,12 +1,4 @@
-import {
-  Claims,
-  ClaimValue,
-  ClaimValueType,
-  FileType,
-  CredentialOffer,
-  isFileClaimValue,
-  isTypedClaimValue
-} from '../common';
+import { Claims, ClaimValue, CredentialOffer, isFileClaimValue } from '../common';
 import * as epam from '../services/epam';
 
 export function createEpamCredentialOffer(offer: CredentialOffer): epam.EpamCreateCredentialsOfferV2 {
@@ -14,64 +6,27 @@ export function createEpamCredentialOffer(offer: CredentialOffer): epam.EpamCrea
     thread_id: offer.threadId,
     name: offer.name,
     description: offer.description,
-    schema_url: offer.contextIri,
-    schema_type: offer.subjectType,
-    attributes: createCredentialAttributes(offer.claims)
+    context_uri: offer.contextUri,
+    schema_uri: offer.schemaUri,
+    subject_type: offer.subjectType,
+    attributes: toAttributes(offer.claims)
   };
 }
 
-function createCredentialAttributes(claims: Claims): epam.CredentialAttributesV2[] {
-  return Object.entries(claims).map(([key, value]) => createCredentialAttribute(key, value));
+function toAttributes(claims: Claims): epam.Attributes {
+  return Object.fromEntries(Object.entries(claims).map(([name, value]) => [name, toAttributeValue(value)]));
 }
 
-function createCredentialAttribute(name: string, claimValue: ClaimValue): epam.CredentialAttributesV2 {
-  if (isFileClaimValue(claimValue)) {
+function toAttributeValue(value: ClaimValue): epam.AttributeValue {
+  if (isFileClaimValue(value)) {
     return {
-      name,
-      value: JSON.stringify({
-        decryption_key: claimValue.decryptionKey,
-        media_type: claimValue.type,
-        sha_512_sum: claimValue.sha512sum,
-        url: claimValue.url
-      }),
-      value_type: toEpamValueType(claimValue.type)
-    };
-  } else if (isTypedClaimValue(claimValue)) {
-    return {
-      name,
-      value: String(claimValue.value),
-      value_type: toEpamValueType(claimValue.type)
+      decryption_key: value.decryptionKey,
+      media_type: value.type,
+      sha_512_sum: value.sha512sum,
+      url: value.url
     };
   } else {
-    return {
-      name,
-      value: String(claimValue),
-      value_type: toEpamValueType(typeof claimValue)
-    };
-  }
-}
-
-function toEpamValueType(type: string | ClaimValueType | FileType): epam.ValueType {
-  switch (type) {
-    case FileType.JPEG:
-      return epam.ValueType.image_jpeg;
-    case FileType.PNG:
-      return epam.ValueType.image_png;
-    case ClaimValueType.Boolean:
-      return epam.ValueType.boolean;
-    case ClaimValueType.Date:
-      return epam.ValueType.date;
-    case ClaimValueType.DateTime:
-      return epam.ValueType.date_time;
-    case ClaimValueType.Integer:
-      return epam.ValueType.integer;
-    case ClaimValueType.Number:
-      return epam.ValueType.number;
-    case ClaimValueType.Time:
-      return epam.ValueType.time;
-    case ClaimValueType.String:
-    default:
-      return epam.ValueType.string;
+    return value;
   }
 }
 
