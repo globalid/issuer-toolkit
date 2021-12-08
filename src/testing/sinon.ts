e9c0eae49a2871406d10d9fd2cb76216bef3a161
Dillon Redding <dillon@global.id>
2021-12-08 17:25:07 -0600

    other(IDENTPD-432): removing default import for sinon

--- src/testing/sinon.ts
+++ src/testing/sinon.ts
@@ -1,16 +1,16 @@
-import sinon from 'sinon';
+import { stub, SinonStub } from 'sinon';
 import GidClient from '..';
 
 export function stubGidClient(clientId = 'default-client-id', clientSecret = 'default-client-secret'): GidClientStub {
   return {
     clientId,
     clientSecret,
-    downloadFile: sinon.stub(GidClient.prototype, 'downloadFile'),
-    getAccessToken: sinon.stub(GidClient.prototype, 'getAccessToken'),
-    reportError: sinon.stub(GidClient.prototype, 'reportError'),
-    sendOffer: sinon.stub(GidClient.prototype, 'sendOffer'),
-    uploadFile: sinon.stub(GidClient.prototype, 'uploadFile'),
-    validateRequest: sinon.stub(GidClient.prototype, 'validateRequest')
+    downloadFile: stub(GidClient.prototype, 'downloadFile'),
+    getAccessToken: stub(GidClient.prototype, 'getAccessToken'),
+    reportError: stub(GidClient.prototype, 'reportError'),
+    sendOffer: stub(GidClient.prototype, 'sendOffer'),
+    uploadFile: stub(GidClient.prototype, 'uploadFile'),
+    validateRequest: stub(GidClient.prototype, 'validateRequest')
   };
 }
 
@@ -21,6 +21,6 @@ export type GidClientStub = {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type AnyFunction = (...args: any[]) => any;
 
-type SinonMethodStub<T extends AnyFunction> = sinon.SinonStub<Parameters<T>, ReturnType<T>>;
+type SinonMethodStub<T extends AnyFunction> = SinonStub<Parameters<T>, ReturnType<T>>;
 
 export default stubGidClient;
