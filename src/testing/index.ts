a30aea7eee6297d1aa39fb2242f360e6fa11b200
Mitja Cotic <mitja.cotic@global.id>
2023-09-28 20:52:53 +0200

    fix(GID-2969): test mock methods accept custom urls

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -5,20 +5,20 @@ export function clearMocks() {
   nock.cleanAll();
 }
 
-export function mockGetAccessToken(accessToken = 'mock.access.token'): void {
-  nock(DEFAULT_BASE_AUTH_URL).post('/realms/globalid/protocol/openid-connect/token').reply(200, {
+export function mockGetAccessToken(accessToken = 'mock.access.token', baseuAuthUrl = DEFAULT_BASE_AUTH_URL): void {
+  nock(baseuAuthUrl).post('/realms/globalid/protocol/openid-connect/token').reply(200, {
     access_token: accessToken
   });
 }
 
-export function mockReportError(): void {
+export function mockReportError(baseSsiUrl = DEFAULT_BASE_SSI_URL): void {
   mockGetAccessToken();
-  nock(DEFAULT_BASE_SSI_URL).post('/v2/aries-management/external-party/credentials/error-reports').reply(204);
+  nock(baseSsiUrl).post('/v2/aries-management/external-party/credentials/error-reports').reply(204);
 }
 
-export function mockSendOffer(error?: string | object): void {
+export function mockSendOffer(error?: string | object, baseSsiUrl = DEFAULT_BASE_SSI_URL): void {
   mockGetAccessToken();
-  const interceptor = nock(DEFAULT_BASE_SSI_URL).post('/v2/aries-management/external-party/credentials/offers');
+  const interceptor = nock(baseSsiUrl).post('/v2/aries-management/external-party/credentials/offers');
   if (error == null) {
     interceptor.reply(204);
   } else {
@@ -26,13 +26,13 @@ export function mockSendOffer(error?: string | object): void {
   }
 }
 
-export function mockUploadFile(gidUuid: string, publicKey: string): void {
-  mockPublicKeyProvider(gidUuid, publicKey);
+export function mockUploadFile(gidUuid: string, publicKey: string, baseApiUrl = DEFAULT_BASE_API_URL): void {
+  mockPublicKeyProvider(gidUuid, publicKey, baseApiUrl);
 
   mockGetAccessToken();
 
   const uploadUrl = 'http://example.com/upload';
-  nock(DEFAULT_BASE_API_URL)
+  nock(baseApiUrl)
     .post('/v2/upload')
     .reply(201, [
       {
@@ -47,16 +47,21 @@ export function mockUploadFile(gidUuid: string, publicKey: string): void {
   nock(uploadUrl).post('/').reply(204);
 }
 
-function mockPublicKeyProvider(gidUuid: string, publicKey: string): void {
-  nock(DEFAULT_BASE_API_URL).get(`/v1/identities/${gidUuid}`).reply(200, {
+function mockPublicKeyProvider(gidUuid: string, publicKey: string, baseApiUrl = DEFAULT_BASE_API_URL): void {
+  nock(baseApiUrl).get(`/v1/identities/${gidUuid}`).reply(200, {
     public_signing_key: publicKey,
     public_encryption_key: publicKey
   });
 }
 
-export function mockValidateRequest(gidUuid: string, publicKey: string): void {
-  mockPublicKeyProvider(gidUuid, publicKey);
-  mockReportError();
+export function mockValidateRequest(
+  gidUuid: string,
+  publicKey: string,
+  baseApiUrl = DEFAULT_BASE_API_URL,
+  baseSsiUrl = DEFAULT_BASE_SSI_URL
+): void {
+  mockPublicKeyProvider(gidUuid, publicKey, baseApiUrl);
+  mockReportError(baseSsiUrl);
 }
 
 export default {
